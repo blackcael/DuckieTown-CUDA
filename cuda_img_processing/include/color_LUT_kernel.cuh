563a8e2c014c882877beae29f9d3cb4f8ca4ba21
blackcael <blackcael@gmail.com>
2025-12-10 12:30:23 -0700

    bug fixes and improvements

--- cuda_img_processing/include/color_LUT_kernel.cuh
+++ cuda_img_processing/include/color_LUT_kernel.cuh
@@ -5,28 +5,29 @@
 #include <stdint.h>
 
 // PARAMS (not used in program, but used in the LUT)
-#define YELLOW_UPPER_THRESH_H 44 
-#define YELLOW_UPPER_THRESH_S 179 
-#define YELLOW_UPPER_THRESH_V 254 
+// #define YELLOW_UPPER_THRESH_H 44 
+// #define YELLOW_UPPER_THRESH_S 179 
+// #define YELLOW_UPPER_THRESH_V 254 
 
-#define YELLOW_LOWER_THRESH_H 16 
-#define YELLOW_LOWER_THRESH_S 51 
-#define YELLOW_LOWER_THRESH_V 91 
+// #define YELLOW_LOWER_THRESH_H 16 
+// #define YELLOW_LOWER_THRESH_S 51 
+// #define YELLOW_LOWER_THRESH_V 91 
 
-#define WHITE_UPPER_THRESH_H 170 
-#define WHTIE_UPPER_THRESH_S 150 
-#define WHITE_UPPER_THRESH_V 255 
+// #define WHITE_UPPER_THRESH_H 170 
+// #define WHTIE_UPPER_THRESH_S 150 
+// #define WHITE_UPPER_THRESH_V 255 
 
-#define WHITE_LOWER_THRESH_H 70 
-#define WHTIE_LOWER_THRESH_S 0 
-#define WHITE_LOWER_THRESH_V 150 
+// #define WHITE_LOWER_THRESH_H 70 
+// #define WHTIE_LOWER_THRESH_S 0 
+// #define WHITE_LOWER_THRESH_V 150
 
 __global__ void color_LUT_kernel(
-    unsigned char* pixel_array, 
+    unsigned char* rgb_pixels_in, 
     int image_height, 
     int image_width,
+    const unsigned char* __restrict__ LUT,
     unsigned char* yellow_out,
-    unsigned char* white_out,
+    unsigned char* white_out
 );
 
 #endif
--- cuda_img_processing/src/color_LUT_kernel.cu
+++ cuda_img_processing/src/color_LUT_kernel.cu
@@ -1,22 +1,33 @@
-#include "rgb_to_grayscale_kernel.cuh"
+#include "color_LUT_kernel.cuh"
+#define NUM_CHANNELS 3
+#define YELLOW_MASK  0b00000010
+#define WHITE_MASK   0b00000001
 
+// can I restrict the input array (pixels in ?)
 
-__global__ void rgb_to_grayscale_kernel(
+__global__ void color_LUT_kernel(
     unsigned char* rgb_pixels_in, 
     int image_height, 
     int image_width,
-    unsigned char* gray_scale_out
+    const unsigned char* __restrict__ LUT,
+    unsigned char* yellow_out,
+    unsigned char* white_out
 ){
     // Calculate indices
     int rowIndex = blockIdx.y * blockDim.y + threadIdx.y;
     int colIndex = blockIdx.x * blockDim.x + threadIdx.x;
     unsigned char inRange = 1;
     if (rowIndex >= image_height || colIndex >= image_width) inRange = 0;
+
+    // Calculate position in image
     if(inRange){
         int pixelIndex = rowIndex * image_width + colIndex;
         unsigned char r = rgb_pixels_in[NUM_CHANNELS * pixelIndex + 0];
         unsigned char g = rgb_pixels_in[NUM_CHANNELS * pixelIndex + 1];
         unsigned char b = rgb_pixels_in[NUM_CHANNELS * pixelIndex + 2];
-        gray_scale_out[pixelIndex] = r * R_WEIGHT + g * G_WEIGHT + b * B_WEIGHT;
+        unsigned char lut_val = LUT[r * 256 * 256 + g * 256 + b];
+        yellow_out[pixelIndex] = YELLOW_MASK & lut_val;
+        white_out[pixelIndex] = WHITE_MASK & lut_val;
     }
+    return;
 }
